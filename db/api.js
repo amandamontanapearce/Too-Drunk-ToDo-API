b894b730bd134be48b947254b15eabdb47ca4ea7
AmandaMontana Pearce <amandamontanapearce@gmail.com>
2016-07-29 14:18:08 -0600

    add routes

--- db/api.js
+++ db/api.js
@@ -9,5 +9,17 @@ module.exports = {
   },
   getAllRows: function(){
     return knex('task').select();
+  },
+  getRowFromPerson: function(personID){
+    return knex('task').select().where({personID: personID});
+  },
+  addTask: function(task, personID) {
+    return knex('task').insert({task: task, started: false, done: false, personID: personID});
+  },
+  editTask: function(id, task, started, done) {
+    return knex('task').where({id: id}).update({ task: task, started: started, done: done});
+  },
+  deleteTask: function(id) {
+    return knex('task').where({id: id}).del();
   }
 }
--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,33 @@ var knex = require('../db/knex');
 var db = require('../db/api');
 
 router.get('/task', function(req, res, next){
-  console.log('hitting')
-  // res.json(db.getAllRows());
+  db.getAllRows().then( function(task){
+    res.json({task: task});
+  })
+})
+
+router.get('/task/:id', function(req, res, next){
+  db.getRowFromPerson(req.params.id).then( function(task){
+    res.json({task: task});
+  })
+})
+
+router.post('/task', function(req, res, next){
+  db.addTask(req.body.task, req.body.personID).then( function(task){
+    res.json("Your task was added, congrats drunkie!");
+  })
+})
+
+router.put('/task/:id', function(req, res, next){
+  db.editTask(req.params.id, req.body.task, req.body.started, req.body.done).then( function(){
+    res.json("task updated!");
+  })
+})
+
+router.delete('/task/:id', function(req, res, next){
+  db.deleteTask(req.params.id).then( function(){
+    res.json("task deleted!")
+  })
 })
 
 module.exports = router;
